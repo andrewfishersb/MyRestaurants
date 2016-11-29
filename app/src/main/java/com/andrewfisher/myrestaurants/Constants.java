6f760079fa29fba524ac6e3e475d93b1617cbfdb
Andrew Fisher <AndrewFishersb@gmail.com>
2016-11-28 21:24:01 -0800

    fix so api call works correctly

--- app/src/main/java/com/andrewfisher/myrestaurants/Constants.java
+++ app/src/main/java/com/andrewfisher/myrestaurants/Constants.java
@@ -3,7 +3,6 @@ package com.andrewfisher.myrestaurants;
 /**
  * Created by andrewfisher on 11/28/16.
  */
-
 public class Constants {
     public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
     public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
@@ -11,6 +10,4 @@ public class Constants {
     public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
     public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
-
-
 }
--- app/src/main/java/com/andrewfisher/myrestaurants/MainActivity.java
+++ app/src/main/java/com/andrewfisher/myrestaurants/MainActivity.java
@@ -1,30 +1,32 @@
 package com.andrewfisher.myrestaurants;
 
+
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
-    private TextView mAppNameTextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        ButterKnife.bind(this);
+
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
 
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(this);
     }
 
